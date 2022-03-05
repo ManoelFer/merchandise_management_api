e77fe5b9178dc1f3f13142ad0428d50a12cf4375
Manoel Fernandes <manoelfernandes15@gmail.com>
2022-03-05 13:40:32 -0300

    ✨ insert validators to user controller

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -4,12 +4,17 @@ import Address from 'App/Models/Address'
 import Role from 'App/Models/Role'
 import User from 'App/Models/User'
 
+import StoreValidator from 'App/Validators/User/StoreValidator'
+import UpdateValidator from 'App/Validators/User/UpdateValidator'
+
 export default class UsersController {
   public async index({ response }: HttpContextContract) {
     response.status(200).json({ message: 'Success' })
   }
 
   public async store({ response, request }: HttpContextContract) {
+    await request.validate(StoreValidator)
+
     const bodyUser = request.only(['name', 'cpf', 'email', 'password'])
     const bodyAddress = request.only([
       'zipCode',
@@ -68,6 +73,8 @@ export default class UsersController {
   }
 
   public async update({ response, request, params }: HttpContextContract) {
+    await request.validate(UpdateValidator)
+
     const userSecureId = params.id
     const bodyUser = request.only(['name', 'cpf', 'email', 'password'])
     const bodyAddress = request.only([
